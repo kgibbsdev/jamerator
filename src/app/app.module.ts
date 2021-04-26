03b3af144f190c38994855b0de8d6417fd39ba26
Richard Nader, Jr <rmnader@svsu.edu>
2021-04-25 22:26:51 -0400

    WIP: Implemented datamuse word api.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { JamgenComponent } from './jamgen/jamgen.component';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { JamgenComponent } from './jamgen/jamgen.component';
     JamgenComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
